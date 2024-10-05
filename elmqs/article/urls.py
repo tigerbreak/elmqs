9d34d29949be1d9ac87bff3b80143fa34be70603
任伟 <15296702060@163.com>
2024-10-05 15:05:04 +0800

    新增了文章的详细页功能

--- elmqs/article/urls.py
+++ elmqs/article/urls.py
@@ -3,4 +3,5 @@ app_name = 'article'
 from . import views
 urlpatterns = [
     path('article-list/', views.article_list, name='article_list'),
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail'),
 ]
--- elmqs/article/views.py
+++ elmqs/article/views.py
@@ -11,3 +11,8 @@ def article_list(request):
     articles = ArticlePost.objects.all()
     context = {'articles': articles}
     return render(request, 'article/list.html', context)
+
+def article_detail(request, id):
+    article = ArticlePost.objects.get(id=id)
+    context = {'article': article}
+    return render(request, 'article/detail.html', context)
